f3217dcb02fa5b297b9be78847001cbdc69e1164
etokoji <etokoji@gmail.com>
2025-12-01 17:55:25 +0900

    Initial Commit

---
+++ photoSelector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  photoSelector
+//
+//  Created by 江藤公二 on 2025/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ photoSelector/photoSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoSelectorApp.swift
+//  photoSelector
+//
+//  Created by 江藤公二 on 2025/12/01.
+//
+
+import SwiftUI
+
+@main
+struct photoSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
